43b88b8a60ae68ab9222d6718ae431b40b8cda1c
Praktik23 <148215700+Praktik23@users.noreply.github.com>
2024-01-10 20:25:53 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const skillsListItem = document.querySelectorAll (".skills-list__item");
+
+    skillsListItem.forEach(item => {
+        const stripe = item.querySelector(".skills-outside__stripe");
+
+        const widthValue = stripe.style.width;
+
+        stripe.setAttribute("data-percent", widthValue);
+    })
+
+    let mixer = mixitup('.portfolio-list');
+    
+});
